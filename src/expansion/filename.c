76d9a23b3881c8695b080f7419f0c8d912434f84
AzerSD <sioudazer8@gmail.com>
2023-06-22 23:27:12 +0200

    filename added some documentation

--- src/expansion/filename.c
+++ src/expansion/filename.c
@@ -15,6 +15,14 @@
 void save_matches(struct s_word **head, struct s_word **tail, glob_t *glob, char **matches);
 void link_to_existing_list(struct s_word **words, struct s_word **pw, struct s_word **head, struct s_word **tail, struct s_word **w);
 
+/**
+ * @brief Perform filename globbing on a linked list of words.
+ * @param words Pointer to the first word in the linked list.
+ * @return The head of the linked list after globbing is completed.
+ *
+ * Filename globbing is the process of expanding wildcard characters 
+ * (`*`, `?`, etc.) in a string to match actual file and directory names.
+*/
 struct s_word *pathnames_expand(struct s_word *words) {
     struct s_word *w;
     struct s_word *pw;
@@ -52,6 +60,16 @@ struct s_word *pathnames_expand(struct s_word *words) {
     return words;
 }
 
+/**
+ * @brief Save matches from globbing into a new linked list.
+ * @param head Pointer to the head of the new linked list.
+ * @param tail Pointer to the tail of the new linked list.
+ * @param glob Pointer to a glob_t struct holding the globbing result.
+ * @param matches Array of matches from globbing.
+ *
+ * This function loops over the matched filenames (`matches`), skipping entries that are `.` or `..` or start with `./`.
+ * It creates a new linked list of words (`head`) from these matches, appending new matches to the end (`tail`) of the list.
+*/
 void save_matches(struct s_word **head, struct s_word **tail, glob_t *glob, char **matches) {
     for (size_t j = 0; j < glob->gl_pathc; j++) {
         if (matches[j][0] == '.' &&
@@ -70,6 +88,19 @@ void save_matches(struct s_word **head, struct s_word **tail, glob_t *glob, char
     }
 }
 
+/**
+ * @brief Link the new list of matches to the existing list of words.
+ * @param words Pointer to the head of the original list of words.
+ * @param pw Pointer to the word previous to the one being globbed in the original list.
+ * @param head Pointer to the head of the new linked list.
+ * @param tail Pointer to the tail of the new linked list.
+ * @param w Pointer to the word being globbed in the original list.
+ *
+ * This function links the new list of matches to the existing list of words. 
+ * If the word being expanded is the first word in the list, the head of the list is changed.
+ * Otherwise, the previous word's `next` pointer is updated to point to the new list.
+ * Finally, the word that was expanded is freed, and `w` is updated to point to the last word that was added (`tail`).
+*/
 void link_to_existing_list(struct s_word **words, struct s_word **pw, struct s_word **head, struct s_word **tail, struct s_word **w) {
     if (*w == *words)
         *words = *head;
